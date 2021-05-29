870317fb908e6d305d46a899918aa9993f906305
Kyle Polich <kyle@dataskeptic.com>
2021-05-28 22:33:47 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import { render } from "react-dom";
+import React, { useReducer, useRef, useTick } from 'react';
 import { Sprite, Stage } from "react-pixi-fiber";
+import { Container, Texture } from '@pixi/display';
 import bunny from "./bunny.png";
-import logo from './logo.svg';
-import './App.css';
 import * as PIXI from "pixi.js";
 
+function Bunny(props) {
+  return <Sprite texture={PIXI.Texture.from(bunny)} {...props} />;
+}
+
 function App() {
-  const props = {x:200, y:200}
-  return (
-    <div className="App">
-  <Stage options={{ backgroundColor: 0x10bb99, height: 600, width: 800 }}>
-  <Sprite texture={PIXI.Texture.from(bunny)} {...props} />
-  </Stage>
-    </div>
-  );
+
+    return (
+        <Stage options={{ backgroundColor: 0x10bb99, height: 600, width: 800 }}>
+          <Bunny x={200} y={200} />
+        </Stage>
+    )
 }
 
 export default App;
